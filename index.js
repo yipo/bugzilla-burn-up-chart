f8e0558936a3aa8944a73517eabe989f7c9d485c
YiPo <yipo.tw@hotmail.com>
2016-09-11 17:17:35 +0800

    Parse the page as an array by the 'cheerio' package

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 var querystring = require('querystring');
 var request = require('request');
+var cheerio = require('cheerio');
 
 function main(site, query) {
   request(site + '/buglist.cgi?' + querystring.stringify(query), (error, response, body) => {
-    console.log(body);
+    var $ = cheerio.load(body);
+
+    var bug_list = $('.bz_id_column').map((index, element) => {
+      return parseInt($(element).children('a').text());
+    }).get();
+
+    console.log(bug_list);
   });
 }
 
