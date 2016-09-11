c267fcd6dea1afa312995b6fa081ffb3c697a2d9
YiPo <yipo.tw@hotmail.com>
2016-09-11 17:07:42 +0800

    Request the bug list page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
+var querystring = require('querystring');
 var request = require('request');
 
-function main(site) {
-  request(site, (error, response, body) => {
+function main(site, query) {
+  request(site + '/buglist.cgi?' + querystring.stringify(query), (error, response, body) => {
     console.log(body);
   });
 }
 
-main(process.argv[2]);
+main(process.argv[2], { product: process.argv[3], target_milestone: process.argv[4] });
 
